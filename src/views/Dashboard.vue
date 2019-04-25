3d11dabe39ae39ec1d77591dbf70bb19f04dd69c
preil <preil2000@gmail.com>
2019-04-25 08:21:52 +0300

    Dashboard tasks data bind.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,30 +2,54 @@
     <div class="Dashboard">
         <h1 class="subheading grey--text">Dashboard</h1>
         <v-container class="my-5">
-            <v-card flat class="pa-3">
-                <v-layout row wrap>
+            <v-card flat v-for="project in projects" :key="project.title">
+                <v-layout row wrap :class="`pa-3 project ${project.status}`">
                     <v-flex xs12 md6>
                         <div class="caption grey--text">Project title</div>
-                        <div>Create new website</div>
+                        <div>{{ project.title }}</div>
                     </v-flex>
                 <v-flex xs6 sm4 md2>
                         <div class="caption grey--text">Person</div>
-                        <div>The Net Ninja</div>
+                        <div>{{ project.person }}</div>
                     </v-flex>
                 <v-flex xs6 sm4 md2>
                         <div class="caption grey--text">Due by</div>
-                        <div>1st Jan 2019</div>
+                        <div>{{ project.due }}</div>
                     </v-flex>
                 <v-flex xs6 sm4 md2>
                         <div class="caption grey--text">Status</div>
-                        <div>ongoing</div>
+                        <div>{{ project.status }}</div>
                     </v-flex>
                 </v-layout>
+                <v-divider></v-divider>
             </v-card>
         </v-container>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        data(){
+            return {
+                projects: [
+                    {title:'Design a new website', person: 'Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum ... '},
+                    {title:'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum ... '},
+                    {title:'Design video thumbnails', person: 'Rye', due: '1st Dec 2018', status: 'overdue', content: 'Lorem ipsum ... '}
+                ]
+            }
+        }
+    }
 </script>
+
+<style>
+    .project.complete {
+        border-left: 4px solid #31d1c2
+    }
+    .project.ongoing {
+        border-left: 4px solid orange
+    }
+    .project.overdue {
+        border-left: 4px solid tomato;
+    }
+
+</style>
