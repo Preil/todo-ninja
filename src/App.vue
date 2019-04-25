3da6361d60d5fa06e3382326d2a32f59a5176be1
preil <preil2000@gmail.com>
2019-04-25 06:39:58 +0300

    Dashboard task added.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <Navbar />
-    <v-content class="mx-4 mb-4">
+    <v-content class="mx-4 mb-4 lighten-4 grey">
       <router-view></router-view>
     </v-content>
   </v-app>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,26 @@
     <div class="Dashboard">
         <h1 class="subheading grey--text">Dashboard</h1>
         <v-container class="my-5">
-            content is here.
+            <v-card flat class="pa-3">
+                <v-layout row wrap>
+                    <v-flex xs12 md6>
+                        <div class="caption grey--text">Project title</div>
+                        <div>Create new website</div>
+                    </v-flex>
+                <v-flex xs6 sm4 md2>
+                        <div class="caption grey--text">Person</div>
+                        <div>The Net Ninja</div>
+                    </v-flex>
+                <v-flex xs6 sm4 md2>
+                        <div class="caption grey--text">Due by</div>
+                        <div>1st Jan 2019</div>
+                    </v-flex>
+                <v-flex xs6 sm4 md2>
+                        <div class="caption grey--text">Status</div>
+                        <div>ongoing</div>
+                    </v-flex>
+                </v-layout>
+            </v-card>
         </v-container>
     </div>
 </template>
