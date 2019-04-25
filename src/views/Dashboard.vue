56e580b8a1e27e7000205c33f2a39733cee0bd19
preil <preil2000@gmail.com>
2019-04-25 08:36:53 +0300

    Using Chips for status

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -8,17 +8,19 @@
                         <div class="caption grey--text">Project title</div>
                         <div>{{ project.title }}</div>
                     </v-flex>
-                <v-flex xs6 sm4 md2>
+                    <v-flex xs6 sm4 md2>
                         <div class="caption grey--text">Person</div>
                         <div>{{ project.person }}</div>
                     </v-flex>
-                <v-flex xs6 sm4 md2>
+                    <v-flex xs6 sm4 md2>
                         <div class="caption grey--text">Due by</div>
                         <div>{{ project.due }}</div>
                     </v-flex>
-                <v-flex xs6 sm4 md2>
-                        <div class="caption grey--text">Status</div>
-                        <div>{{ project.status }}</div>
+                    <v-flex xs2 sm4 md2>
+                        <div class="right">
+                            <v-chip small :class="`${project.status} white--text caption my-2`">{{ project.status }}
+                            </v-chip>
+                        </div>
                     </v-flex>
                 </v-layout>
                 <v-divider></v-divider>
@@ -29,12 +31,30 @@
 
 <script>
     export default {
-        data(){
+        data() {
             return {
                 projects: [
-                    {title:'Design a new website', person: 'Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum ... '},
-                    {title:'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum ... '},
-                    {title:'Design video thumbnails', person: 'Rye', due: '1st Dec 2018', status: 'overdue', content: 'Lorem ipsum ... '}
+                    {
+                        title: 'Design a new website',
+                        person: 'Ninja',
+                        due: '1st Jan 2019',
+                        status: 'ongoing',
+                        content: 'Lorem ipsum ... '
+                    },
+                    {
+                        title: 'Code up the homepage',
+                        person: 'Chun Li',
+                        due: '10th Jan 2019',
+                        status: 'complete',
+                        content: 'Lorem ipsum ... '
+                    },
+                    {
+                        title: 'Design video thumbnails',
+                        person: 'Rye',
+                        due: '1st Dec 2018',
+                        status: 'overdue',
+                        content: 'Lorem ipsum ... '
+                    }
                 ]
             }
         }
@@ -45,11 +65,25 @@
     .project.complete {
         border-left: 4px solid #31d1c2
     }
+
     .project.ongoing {
         border-left: 4px solid orange
     }
+
     .project.overdue {
         border-left: 4px solid tomato;
+    }
+
+    .v-chip.complete {
+        background: #31d1c2
+    }
+
+    .v-chip.ongoing {
+        background: orange
+    }
+
+    .v-chip.overdue {
+        background: tomato
     }
 
 </style>
