1f0493b4e6738890678df053b7f9cdec37b365fd
preil <preil2000@gmail.com>
2019-04-26 17:43:48 +0300

    Expansion panel example.

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,8 +1,57 @@
 <template>
-  <div class="projects">
-    <h1 class="subheading grey--text">Projects</h1>
-    <v-container class="my-5">
-      content is here.
-    </v-container>
-  </div>
+    <div class="projects">
+        <h1 class="subheading grey--text">Projects</h1>
+        <v-container class="my-5">
+            <v-expansion-panel>
+                <v-expansion-panel-content v-for="project in projects" :key="project.title">
+                    <div slot="header">{{ project.title }}</div>
+                    <v-card>
+                        <v-card-text class="px-4 grey--text">
+                            <div class="font-weight-bold">due by</div>
+                            <div>{{ project.content }}</div>
+                        </v-card-text>
+                    </v-card>
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+        </v-container>
+    </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                projects: [
+                    {
+                        title: 'Design a new website',
+                        person: 'Ninja',
+                        due: '1st Jan 2019',
+                        status: 'ongoing',
+                        content: 'Lorem ipsum ... '
+                    },
+                    {
+                        title: 'Zip up the homepage',
+                        person: 'Chun Li',
+                        due: '10th Jan 2019',
+                        status: 'complete',
+                        content: 'Lorem ipsum ... '
+                    },
+                    {
+                        title: 'Design video thumbnails',
+                        person: 'Rye',
+                        due: '1st Dec 2018',
+                        status: 'overdue',
+                        content: 'Lorem ipsum ... '
+                    }
+                ]
+            }
+        },
+        computed: {
+            myProjects() {
+                return this.projects.filter(project => {
+                    return project.person === 'Ninja'
+                })
+            }
+        }
+    }
+</script>
