31dc376ec74d791bc95cd78b2f4a4612dbf350f8
Preil <preil2000@gmail.com>
2019-07-23 15:42:17 +0300

    Firebase installed and config created.

---
+++ src/fb.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+var config = {
+    apiKey: "AIzaSyBPz49UALnFBuwClO7MFknJ7pVDt5jRlkA",
+    authDomain: "todo-ninja-5c8ac.firebaseapp.com",
+    databaseURL: "https://todo-ninja-5c8ac.firebaseio.com",
+    projectId: "todo-ninja-5c8ac",
+    storageBucket: "todo-ninja-5c8ac.appspot.com",
+    messagingSenderId: "1079653090174",
+    appId: "1:1079653090174:web:be5443682251cd01"
+}
+firebase.initializeApp(config)
+const db = firebase.firestore()
+db.settings({timestampsInSnapshots: true})
+
+export default db
